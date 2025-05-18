55ca6cec082f413dba82086800a10885eadbdb23
Ana Luisa <anadrvv@gmail.com>
2025-05-18 15:42:16 -0300

    pages

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+fetch('/pages/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+  })
+  .catch(error => console.error('Erro ao carregar o header:', error));
