6d65adc92b4153b51042c4cc69053b17432e41ad
Ana Luisa <anadrvv@gmail.com>
2025-05-20 18:28:26 -0300

    about us

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,30 @@ fetch('/pages/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
+
+
+    destacarLinkAtivo();
   })
   .catch(error => console.error('Erro ao carregar o header:', error));
 
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("mostrar");
+});
+
 function abrirLink(url) {
-    window.open(url, "_blank");
-  }
+  window.open(url, "_blank");
+}
+
+
+function destacarLinkAtivo() {
+  const path = window.location.pathname;
+  const links = document.querySelectorAll("nav a");
+
+  if (path === "/" || path === "/index.html") return;
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === path) {
+      link.classList.add("active");
+    }
+  });
+}
