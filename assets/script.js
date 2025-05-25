35a188218bc6db8ff002dc306624e449143d8e75
Ana Luisa <anadrvv@gmail.com>
2025-05-24 21:45:29 -0300

    mobile

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,30 @@ window.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("mostrar");
 });
 
+fetch('/pages/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => console.error('Erro ao carregar o footer:', error));
+
+  fetch('/pages/mobile-nav.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('mobile-nav-placeholder').innerHTML = data;
+
+
+    const toggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.mobile-menu');
+
+    if (toggle && menu) {
+      toggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+      });
+    }
+  })
+  .catch(error => console.error('Erro ao carregar o menu mobile:', error));
+
 function abrirLink(url) {
   window.open(url, "_blank");
 }
