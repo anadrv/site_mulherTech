557ab0a4fb278c6a09647b9204bc4ff0f2cbf4c1
Ana Luisa <anadrvv@gmail.com>
2025-05-27 22:37:02 -0300

    nav

--- assets/script.js
+++ assets/script.js
@@ -53,3 +53,44 @@ function destacarLinkAtivo() {
     }
   });
 }
+
+const events = [
+  {
+    date: "01",
+    month: "abril",
+    title: "Café Colaborativo",
+    desc: "Encontro para conversa e acolhimento das novas integrantes na área de TI da faculdade.",
+    img: "/assets/images/event-image.png"
+  },
+  {
+    date: "15",
+    month: "maio",
+    title: "Workshop Front-end",
+    desc: "Oficina prática de HTML, CSS e JS voltada para iniciantes.",
+    img: "/assets/images/junhan.png"
+  },
+  {
+    date: "30",
+    month: "junho",
+    title: "Papo Tech",
+    desc: "Bate-papo com profissionais da área sobre carreira e tecnologia.",
+    img: "/assets/images/podcast.jpg"
+  }
+];
+
+
+let current = 0;
+
+const dateEl = document.querySelector(".date h3");
+const monthEl = document.querySelector(".date p");
+const descEl = document.querySelector(".description");
+const imageEl = document.querySelector(".event-img");
+
+document.querySelector(".seta").addEventListener("click", () => {
+  current = (current + 1) % events.length;
+
+  dateEl.textContent = events[current].date;
+  monthEl.textContent = events[current].month;
+  descEl.innerHTML = `<strong>${events[current].title}</strong> - ${events[current].desc}`;
+  imageEl.src = events[current].img;
+});
